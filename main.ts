0c73ff2ccb1065e13123f476d2839c8775b4b508
jh5g <31213220+jh5g@users.noreply.github.com>
2025-09-16 21:12:22 +0100

    First draft

--- main.ts
+++ main.ts
@@ -14,121 +14,54 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+        this.addCommand({
+            id: "schedule-alarms",
+            name: "Schedule alarms from table",
+            callback: () => this.scheduleAlarms(),
+        });
 	}
 
 	onunload() {
-
+        console.log("Unloading Alarm Plugin");
 	}
+    async scheduleAlarms() {
+        const file = this.app.workspace.getActiveFile();
+        if (!file) return;
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+        const content = await this.app.vault.read(file);
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+        // Step 1: Extract times from the markdown
+        const times = this.extractTimes(content);
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+        // Step 2: Schedule alarms
+        times.forEach((time) => this.setAlarm(time));
+    }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    extractTimes(content: string): string[] {
+        const regex = /\b([01]?\d|2[0-3]):([0-5]\d)\b/g; // matches HH:MM
+        return [...content.matchAll(regex)].map(match => match[0]);
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    setAlarm(timeStr: string) {
+        const [hours, minutes] = timeStr.split(":").map(Number);
+        const now = new Date();
+        const target = new Date();
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        target.setHours(hours, minutes, 0, 0);
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+        // If the time has already passed today, skip
+        if (target < now) return;
 
-	display(): void {
-		const {containerEl} = this;
+        const delay = target.getTime() - now.getTime();
 
-		containerEl.empty();
+        setTimeout(() => {
+            this.playAlarm();
+            new Notice(`⏰ Alarm for ${timeStr}`);
+        }, delay);
+    }
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    playAlarm() {
+        const audio = new Audio("https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg");
+        audio.play();
+    }
 }
